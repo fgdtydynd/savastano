193ece5e3c3ee01fa3650bf3fd26e120dca603ad
fgdtydynd <53189602+fgdtydynd@users.noreply.github.com>
2019-09-01 01:28:28 +0200

    Update help.js

--- Commandes/help.js
+++ Commandes/help.js
@@ -4,15 +4,15 @@ const color = require("../utils/color.json");
 
 module.exports.run = async(client, message, args) => {
 	var embed1 = new Discord.RichEmbed()
-		.setTitle("👉San Andreas Rp👈")
+		.setTitle("San Andreas Rp")
 		.setColor(color.black)
-		.setTitle("👉San Andreas Rp👈")
+		.setTitle("San Andreas Rp")
 		.setColor(color.black)
 		.setThumbnail('https://cdn.discordapp.com/attachments/600741152982827009/607909446030721034/JPEG_20190727_170515.jpg')
-		.addField("☀️ Commande RP :", `${config.prefix}chuchoter, ${config.prefix}crie, ${config.prefix}parler`)
-		.addField(":gear: Commande admin :", `${config.prefix}rp, ${config.prefix}mpall, ${config.prefix}say, ${config.prefix}clear`)
-		.addField("🕶️ Commande divers :", ` ${config.prefix}ping`)
-		.setDescription("bon RP a tous ")
+		.addField("🥀 Commande RP 🥀:", `${config.prefix}chuchoter, ${config.prefix}crie, ${config.prefix}parler`)
+		.addField("🔰 Commande admin 🔰:", `${config.prefix}rp, ${config.prefix}mpall, ${config.prefix}say, ${config.prefix}clear`)
+		.addField("💎 Commande divers 💎:", ` ${config.prefix}ping`)
+
 		.setImage('https://cdn.discordapp.com/attachments/600741152982827009/607903023309783040/20190724_211825-1.gif')
 		.setFooter(`${client.user.username} By LeStyle`)
 
